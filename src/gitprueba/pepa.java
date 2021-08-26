2f0ad16b3ca06628cf0ebf18f6b97386601cbf45
Makangel <sgroangie@gmail.com>
2021-08-25 22:08:19 -0300

    agrege clase pepa

---
+++ src/gitprueba/pepa.java
@@ -0,0 +1,18 @@
+/*
+*
+*/
+
+package gitprueba;
+
+public class pepa {
+    private String nombre ="PEPA";
+    
+    private String ocupacion ="Novia del pepe";
+    private String edad ="Indefinida";
+    
+    public void presentarse(){
+        System.out.println("Hola mi nombre es :"+nombre);
+        System.out.println("Mi ocupacion es: "+ocupacion);
+        System.out.println("Mi edad es: "+edad);
+    }
+}
